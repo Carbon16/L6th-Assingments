6d29c9de02084f45310328ff0d9325b6adb5f3fa
Carbon1769 <92681803+Carbon16@users.noreply.github.com>
2022-09-10 10:41:24 +0100

    grades

--- Selection test/Module1.vb
+++ Selection test/Module1.vb
@@ -1,7 +1,23 @@
-﻿Module Module1
+﻿Imports System.Console
+
+Module Module1
 
     Sub Main()
+        Dim score As Integer
 
+        Write("Enter score: ") : score = ReadLine()
+
+        Select Case score
+            Case >= 90 : WriteLine("A*")
+            Case >= 80 : WriteLine("A")
+            Case >= 70 : WriteLine("B")
+            Case >= 60 : WriteLine("C")
+            Case >= 50 : WriteLine("D")
+            Case >= 40 : WriteLine("E")
+            Case Else : WriteLine("U")
+        End Select
+
+        ReadLine()
     End Sub
 
 End Module
