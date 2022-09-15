0cb6d61c862bb162f413d2713a162aebaa4b38b6
Carbon1769 <92681803+Carbon1769@users.noreply.github.com>
2022-09-15 21:08:01 +0100

    added task 1

--- Built in functions (part 2)/Module1.vb
+++ Built in functions (part 2)/Module1.vb
@@ -1,8 +1,24 @@
 ﻿Imports System.Console
 
 Module Module1
+    Sub Task1()
+        'PROBLEM 1
+        WriteLine("----------------PROBLEM 1----------------")
+        Dim len, deep, shall, wid, vol As Integer
+        WriteLine("Enter all measurments in Meters")
+        Write("Enter length: ") : len = ReadLine()
+        Write("Enter length of deep end: ") : deep = ReadLine()
+        Write("Enter depth of shallow end: ") : shall = ReadLine()
+        Write("Enter width: ") : wid = ReadLine()
+
+        vol = (((((deep * shall) * 0.5) * len) * wid) * 1000)
+
+        WriteLine("The swimming pool holds " & vol & " litres.")
+        ReadLine()
+    End Sub
 
     Sub Main()
+        Task1()
         'PROBLEM 4
         WriteLine("----------------PROBLEM 4----------------")
 
