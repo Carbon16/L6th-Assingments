0b82e11fa38933beeb484cae1f4633ffeb9109bb
Carbon1769 <92681803+Carbon1769@users.noreply.github.com>
2022-09-22 22:14:39 +0100

    YEET

---
+++ Iteration-Prep/Module1.vb
@@ -0,0 +1,64 @@
+﻿'Author: Leo Skingley
+'Date: 22/09/22
+'GitHub: Carbon16/L6th-Assingments
+'Description: Loop and iteration practice prep
+
+Imports System.Console
+
+Module Module1
+
+    Sub Main()
+        Try
+            WriteLine("----------------Task 1--------------------")
+            Write("Enter password: ") : Dim pwd As String = ReadLine()
+            While Not pwd = "secret"
+                WriteLine("Incorrect password, try again!")
+                Write("Enter password: ") : pwd = ReadLine()
+            End While
+            WriteLine("You are in!")
+
+            ReadLine()
+
+            Task2()
+        Catch Ex As Exception
+            WriteLine("Error!")
+        End Try
+        ReadLine()
+    End Sub
+
+    Sub Task2()
+        WriteLine("----------------Task 1--------------------")
+        Dim num, high, low As Integer : high = -1 : low = -1 : num = 0
+        While Not num < 0
+            If num > high Then : high = num
+            ElseIf num < low AndAlso num > 0 Then : low = num
+            End If
+            Write("Enter number: ") : num = ReadLine()
+        End While
+
+        WriteLine("Highest number: " & high)
+        WriteLine("Lowest number: " & low)
+
+        ReadLine()
+        Task3()
+    End Sub
+
+    Sub Task3()
+        WriteLine("----------------Task 3--------------------")
+
+        'COULD NOT COMPLETE, ATTEMPED IN PYHON.
+
+        'Write("What number would you like to go up to: ") : Dim upTo As Integer = ReadLine()
+
+        'For i As Integer
+        'For i As Integer = 0 To upTo
+        '    WriteLine()
+        'Next
+
+        ''Console.WriteLine("{0,-20} {1,-10} {2,-10}", "{column 1}", "{column 2}", "")
+        ''Console.WriteLine("{0,-20} {1,-10} {2,-10}", "0", 0, 0)
+
+        ReadLine()
+    End Sub
+
+End Module
--- prep-1/Module1.vb
+++ prep-1/Module1.vb
@@ -8,46 +8,47 @@ Imports System.Console
 Module Module1
 
     Sub Main()
-        Dim leave As Bool = False
+        Dim leave As Boolean = False
         While leave = False
-        Try
-            Clear()
-            WriteLine("1. Check if number is even or odd\n2. Number of letters and number of 'a's\n3. Find the number of words in a sentence\n4. Do a calculation\n5. Exit)")
-            Dim menuSelect As Integer = Readline()
-            Select Case menuSelect
-                Case 1 : evOd()
-                Case 2 : charCount()
-                Case 3 : wordCount()
-                Case 4 : cal()
-                Case 5 : WriteLine("Goodbie!") : leave = True
-            End Select
-        Catch Ex as Exception
-            Clear()
-            WriteLine("Error!")
-        End Try
+            Try
+                Clear()
+                WriteLine("1. Check if number is even or odd    2. Number of letters and number of 'a's     3. Find the number of words in a sentence       4. Do a calculation     5. Exit")
+                Dim menuSelect As Integer = ReadLine()
+                Select Case menuSelect
+                    Case 1 : evOd()
+                    Case 2 : charCount()
+                    Case 3 : wordCount()
+                    Case 4 : cal()
+                    Case 5 : WriteLine("Goodbie!") : leave = True
+                End Select
+            Catch Ex As Exception
+                Clear()
+                WriteLine("Error!")
+            End Try
 
-        ReadLine()
+            ReadLine()
         End While
     End Sub
 
     Sub evOd()
         Clear()
-        Dim num1, num2 as Integer
-        Write("Number one: ") : num1 = ReadLine()
-        
-        If num1 Mod 2 = 0 : WriteLine("Number is even!")
-        If num1 Mod 2 != 0 : WriteLine("Number is odd!")
+        Write("Number one: ") : Dim num1 As Integer = ReadLine()
+
+        If num1 Mod 2 = 0 Then : WriteLine("Number is even!")
+        Else : WriteLine("Number is odd!")
+        End If
 
         ReadLine()
+
     End Sub
 
     Sub charCount()
         Clear()
         Write("Enter sentence: ")
-        Dim sentence as String = Readline()
+        Dim sentence As String = ReadLine()
 
         WriteLine("Sentence has " & Len(sentence) & " chars.")
-        WriteLine("Sentence has " & sentence.Split("a").Length -1 & " a chars."
+        WriteLine("Sentence has " & sentence.Split("a").Length - 1 & " a chars.")
 
         ReadLine()
     End Sub
@@ -64,7 +65,7 @@ Module Module1
         Clear()
         Write("Enter an operator: ") : Dim operand As String = ReadLine()
         Write("Enter a number: ") : Dim num1 As Integer = ReadLine()
-        Write("Enter another number: ") : Dim num2 as Integer = ReadLine()
+        Write("Enter another number: ") : Dim num2 As Integer = ReadLine()
 
         Select Case operand
             Case "*" : WriteLine("Result: " & num1 * num2)
